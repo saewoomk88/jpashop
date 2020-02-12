23fe9a392ae1338238f6706ce709fb7fa9e37df9
saewoomk88 <43767934+saewoomk88@users.noreply.github.com>
2020-02-12 17:16:40 +0900

    Initial commit

---
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop.domain.item;
+
+import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+@Getter @Setter
+public abstract class Item {
+
+    @Id @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<>();
+
+    //==비지니스 로직== 객체지향적으로 생각을 해보면 데이터를 가지고 있는 쪽에
+    // 비니니스 메서드를 가지고 있는게 좋음 응집력이 있음
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if (restStock<0){
+            throw new NotEnoughStockException("Need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
+}
